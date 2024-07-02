0e2c6cd4dad749bc5122c1c3543b067c341b1a42
harishsambasivam <imharishsambasivam@gmail.com>
2024-07-02 05:57:11 +0530

    feat: common db utilities

--- src/db.js
+++ src/db.js
@@ -7,29 +7,23 @@ class DB {
   }
 
   connect = async (options) => {
-    try {
-      switch (this.database) {
-        case "mongodb":
-          await mongoose.connect(this.uri, {
-            serverSelectionTimeoutMS: 5000,
-            socketTimeoutMS: 5000,
-            ...options,
-          });
-          logger.info("successfully connected with mongodb");
-          break;
+    switch (this.database) {
+      case "mongodb":
+        await mongoose.connect(this.uri, {
+          serverSelectionTimeoutMS: 5000,
+          socketTimeoutMS: 5000,
+          ...options,
+        });
+        logger.info("successfully connected with mongodb");
+        break;
 
-        case "postgres":
-          logger.info("successfully connected with postgres");
+      case "postgres":
+        logger.info("successfully connected with postgres");
+        break;
 
-          break;
-
-        case "redis":
-          logger.info("successfully connected with redis");
-          break;
-      }
-    } catch (err) {
-      logger.error(err);
-      process.exit(1);
+      case "redis":
+        logger.info("successfully connected with redis");
+        break;
     }
   };
 }
